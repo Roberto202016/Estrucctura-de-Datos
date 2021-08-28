3b7b58fc46b59ef056a855bf71d693f1e3984d11
ProfeEgel <45474206+ProfeEgel@users.noreply.github.com>
2021-08-28 01:03:00 -0500

    PooBasics project added

---
+++ PooBasics/PooBasics/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primer programa de consola en C#");
+
+            String.Compare("", "");
+            String texto = "Hola";
+        }
+    }
+}
