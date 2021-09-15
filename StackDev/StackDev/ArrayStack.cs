c7a4422a514dbcbfd2e9cf0c8fb1211ada028f95
ProfeEgel <45474206+ProfeEgel@users.noreply.github.com>
2021-09-15 11:36:54 -0500

    StackDev updated

--- StackDev/StackDev/ArrayStack.cs
+++ StackDev/StackDev/ArrayStack.cs
@@ -10,19 +10,35 @@ namespace DtsLib
     {
         private T[] data;
         private int index;
+        private readonly int initialCapacity;
 
+        public int Capacity { get; private set; }
         public int Size => index + 1;
 
         public bool Empty => index == -1;
 
-        public ArrayStack(int size)
+        public ArrayStack(int capacity)
         {
-            data = new T[size];
+            data = new T[capacity];
+            initialCapacity = capacity;
+            Capacity = capacity;
             index = -1;
         }
 
         public void Push(T item)
         {
+            if (Size == Capacity)
+            {
+                Array.Resize<T>(ref data, Capacity * 2);
+                Capacity *= 2;
+
+                /*
+                T[] newData = new T[Capacity * 2];
+                Array.Copy(data, newData, Capacity);
+                data = newData;
+                */
+            }
+
             data[index + 1] = item;
             index++;
         }
@@ -39,5 +55,19 @@ namespace DtsLib
         }
 
         public T Peek() => data[index];
+
+        public override string ToString()
+        {
+            string str = $"C:{Capacity}, S:{Size}, E:{Empty}";
+
+            str += "\n[";
+            for (int i = 0; i <= index; ++i)
+            {
+                str += $" {data[i]}";
+            }
+            str += " ]\n";
+
+            return str;
+        }
     }
 }
--- StackDev/StackDev/Program.cs
+++ StackDev/StackDev/Program.cs
@@ -5,21 +5,48 @@ using System.Text;
 using System.Threading.Tasks;
 using DtsLib;
 
+using static System.Console;
+
 namespace StackDev
 {
     class Program
     {
         static void Main(string[] args)
         {
-            IStack<int> stack = new ArrayStack<int>(5);
+            ArrayStack<int> stack = new ArrayStack<int>(2);
+            WriteLine(stack);
             
             stack.Push(5);
+            WriteLine(stack);
+
             stack.Push(13);
-            Console.WriteLine(stack.Pop());
+            WriteLine(stack);
+
+            WriteLine(stack.Pop());
+            WriteLine(stack);
+
             stack.Push(-3);
             stack.Push(25);
-            Console.WriteLine(stack.Peek());
-            Console.WriteLine(stack.Pop());
+            WriteLine(stack.Peek());
+            WriteLine(stack);
+
+            WriteLine(stack.Pop());
+            WriteLine(stack);
+
+            //stack.Pop();
+            //stack.Pop();
+            //stack.Pop();
+
+            stack.Push(2);
+            stack.Push(25);
+            stack.Push(57);
+            WriteLine(stack);
+
+            stack.Push(-87);
+            stack.Push(5);
+            stack.Push(3);
+            stack.Push(1);
+            WriteLine(stack);
         }
     }
 }
