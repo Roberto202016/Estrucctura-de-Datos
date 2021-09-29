b91b05015f601bb44ad7cdd3eea910a1e1069cae
ProfeEgel <45474206+ProfeEgel@users.noreply.github.com>
2021-09-29 15:39:34 -0500

    QueueDev project updated

---
+++ QueueDev/QueueDev/CircularQueue.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtsLib
+{
+    class CircularQueue<T> : IQueue<T>
+    {
+        private T[] data;
+        private int t;
+        private int h;
+
+        public int Capacity { get; }
+        public int Size { get; private set; }
+
+        public bool Empty => Size == 0;
+
+        public CircularQueue(int capacity)
+        {
+            data = new T[capacity];
+            Capacity = capacity;
+            Size = 0;
+            h = 0;
+            t = 0;
+        }
+
+        public void Enqueue(T item)
+        {
+            if (Size == Capacity) { throw new InvalidOperationException(); }
+
+            
+        }
+
+        public T Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Head()
+        {
+            if (Empty) { throw new InvalidOperationException(); }
+            return data[h];
+        }
+
+        public T Tail()
+        {
+            if (Empty) { throw new InvalidOperationException(); }
+            return data[t];
+        }
+    }
+}
